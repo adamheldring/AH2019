c4edfa6dc0ab3a4359a940633fa77508b5fc6ce7
A-Game <javascript@adamheldring.com>
2019-09-30 21:07:46 +0200

    animate each mobile menu item on enter

--- code/src/components/MobileMenu/MobileMenu.jsx
+++ code/src/components/MobileMenu/MobileMenu.jsx
@@ -1,39 +1,49 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { MdClose } from 'react-icons/md'
-import { useSpring, animated } from 'react-spring'
+import { useSpring, useTrail, animated } from 'react-spring'
 import SocialMediaBar from '../SocialMediaBar/SocialMediaBar'
 import menuData from '../../../data/menuData'
 
-const MobileMenu = ({ setMobileMenuOpen }) => (
-  <div className="mobile-menu">
-    <div className="mobile-menu__header">
-      <span>AH2019</span>
-      <button
-        type="button"
-        className="mobile-menu__button"
-        onClick={() => setMobileMenuOpen(false)}
-      >
-        <MdClose />
-      </button>
+const MobileMenu = ({ setMobileMenuOpen }) => {
+  const menuItems = menuData
+  const trail = useTrail(menuItems.length, {
+    opacity: 1,
+    from: { opacity: 0 },
+  })
+  return (
+    <div className="mobile-menu">
+      <div className="mobile-menu__header">
+        <span>AH2019</span>
+        <button
+          type="button"
+          className="mobile-menu__button"
+          onClick={() => setMobileMenuOpen(false)}
+        >
+          <MdClose />
+        </button>
+      </div>
+      <ul>
+        {trail.map((props, index) => (
+          <animated.div
+            key={menuItems[index].title}
+            style={{ ...props, overflow: 'hidden' }}
+          >
+            <MenuItem menuItem={menuItems[index]} index={index} />
+          </animated.div>
+        ))}
+      </ul>
+      <SocialMediaBar />
+      <div className="mobile-menu__submenu">
+        <span className="mobile-menu__submenu-item">CONTACT</span> |{' '}
+        <span className="mobile-menu__submenu-item">ABOUT</span>
+      </div>
     </div>
-    <ul>
-      {menuData.map((menuItem, index) => (
-        <MenuItem menuItem={menuItem} index={index} />
-      ))}
-    </ul>
-    <SocialMediaBar />
-    <div className="mobile-menu__submenu">
-      <span className="mobile-menu__submenu-item">CONTACT</span> |{' '}
-      <span className="mobile-menu__submenu-item">ABOUT</span>
-    </div>
-  </div>
-)
-
-export default MobileMenu
+  )
+}
 
 const MenuItem = ({ menuItem, index }) => {
-  const listItemStyle = useSpring({ opacity: 1, from: { opacity: 0 } })
+  const listItemStyle = useSpring({ color: '#333', from: { color: '#eeeeee' } })
   return (
     <li key={`${index}-${menuItem.title}`}>
       <Link to={menuItem.url} onClick={() => setMobileMenuOpen(false)}>
@@ -47,3 +57,5 @@ const MenuItem = ({ menuItem, index }) => {
     </li>
   )
 }
+
+export default MobileMenu
