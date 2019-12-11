ab8b8952b14d16f2351f2330ad6929e5f85746fe
A-Game <javascript@adamheldring.com>
2019-12-11 16:30:08 +0100

    Move mobile menu button and adjust desktop menu details

--- code/src/components/header.js
+++ code/src/components/header.js
@@ -7,27 +7,29 @@ import './MobileMenu/MobileMenu.sass'
 const Header = ({ siteTitle, setMobileMenuOpen }) => (
   <header className="ah-header">
     <div>
-      <h1 className="ah-header-title ah-header-title-left">
-        <Link to="/" className="ah-header-title-link">
-          {/* {siteTitle} */}A
-          <span className="ah-header-title-left-da-kerning">D</span>AM
-        </Link>
-      </h1>
+      <div className="ah-header-title-upper-wrapper">
+        <h1 className="ah-header-title ah-header-title-left">
+          <Link to="/" className="ah-header-title-link">
+            {/* {siteTitle} */}A
+            <span className="ah-header-title-left-da-kerning">D</span>AM
+          </Link>
+        </h1>
+        <div className="ah-header-mobile-menu-button-wrapper">
+          <button
+            type="button"
+            className="mobile-menu__button"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <MdMenu />
+          </button>
+        </div>
+      </div>
       <h1 className="ah-header-title ah-header-title-right">
         <Link to="/" className="ah-header-title-link">
           {/* {siteTitle} */}
           HELDRING
         </Link>
       </h1>
-    </div>
-    <div className="ah-header-mobile-menu-button-wrapper">
-      <button
-        type="button"
-        className="mobile-menu__button"
-        onClick={() => setMobileMenuOpen(true)}
-      >
-        <MdMenu />
-      </button>
     </div>
   </header>
 )
