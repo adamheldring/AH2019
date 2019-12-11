64d9412b73156ec13de2415eb801e6f477b7b611
A-Game <javascript@adamheldring.com>
2019-12-11 17:32:13 +0100

    animate site title on load 2

--- code/src/components/header.js
+++ code/src/components/header.js
@@ -2,37 +2,54 @@ import React from 'react'
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import { MdMenu } from 'react-icons/md'
+import { useSpring, animated } from 'react-spring'
 import './MobileMenu/MobileMenu.sass'
 
-const Header = ({ siteTitle, setMobileMenuOpen }) => (
-  <header className="ah-header">
-    <div>
-      <div className="ah-header-title-upper-wrapper">
-        <h1 className="ah-header-title ah-header-title-left">
-          <Link to="/" className="ah-header-title-link">
-            {/* {siteTitle} */}A
-            <span className="ah-header-title-left-da-kerning">D</span>AM
-          </Link>
-        </h1>
-        <div className="ah-header-mobile-menu-button-wrapper">
-          <button
-            type="button"
-            className="mobile-menu__button"
-            onClick={() => setMobileMenuOpen(true)}
+const Header = ({ siteTitle, setMobileMenuOpen }) => {
+  const titleLeftStyle = useSpring({
+    left: '0px',
+    from: { left: '-100%' },
+  })
+  const titleRightStyle = useSpring({
+    right: '0px',
+    from: { right: '-100%' },
+  })
+  return (
+    <header className="ah-header">
+      <div>
+        <div className="ah-header-title-upper-wrapper">
+          <animated.h1
+            className="ah-header-title ah-header-title-left"
+            style={titleLeftStyle}
           >
-            <MdMenu />
-          </button>
+            <Link to="/" className="ah-header-title-link">
+              {/* {siteTitle} */}A
+              <span className="ah-header-title-left-da-kerning">D</span>AM
+            </Link>
+          </animated.h1>
+          <div className="ah-header-mobile-menu-button-wrapper">
+            <button
+              type="button"
+              className="mobile-menu__button"
+              onClick={() => setMobileMenuOpen(true)}
+            >
+              <MdMenu />
+            </button>
+          </div>
         </div>
+        <animated.h1
+          className="ah-header-title ah-header-title-right"
+          style={titleRightStyle}
+        >
+          <Link to="/" className="ah-header-title-link">
+            {/* {siteTitle} */}
+            HELDRING
+          </Link>
+        </animated.h1>
       </div>
-      <h1 className="ah-header-title ah-header-title-right">
-        <Link to="/" className="ah-header-title-link">
-          {/* {siteTitle} */}
-          HELDRING
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
