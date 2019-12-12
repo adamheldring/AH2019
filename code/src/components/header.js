cc5a4774df1a55d3f3744ad5573eaa67cf97dbfc
A-Game <javascript@adamheldring.com>
2019-12-12 01:20:57 +0100

    add intersection observer title animation

--- code/src/components/header.js
+++ code/src/components/header.js
@@ -3,21 +3,28 @@ import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import { MdMenu } from 'react-icons/md'
 import { useSpring, animated } from 'react-spring'
+import { useInView } from 'react-intersection-observer'
 import './MobileMenu/MobileMenu.sass'
 
 const Header = ({ siteTitle, setMobileMenuOpen }) => {
+  const [titleUpperRef, titleUpperInView, titleUpperEntry] = useInView({
+    threshold: 0.5,
+  })
+  const [titleLowerRef, titleLowerInView, titleLoweEntry] = useInView({
+    threshold: 0.5,
+  })
   const titleLeftStyle = useSpring({
-    left: '0px',
+    left: `${titleUpperInView ? '0' : '-100%'}`,
     from: { left: '-100%' },
   })
   const titleRightStyle = useSpring({
-    right: '0px',
-    from: { right: '-100%' },
+    left: `${titleLowerInView ? '0' : '100%'}`,
+    from: { right: '100%' },
   })
   return (
     <header className="ah-header">
       <div>
-        <div className="ah-header-title-upper-wrapper">
+        <div className="ah-header-title-upper-wrapper" ref={titleUpperRef}>
           <animated.h1
             className="ah-header-title ah-header-title-left"
             style={titleLeftStyle}
@@ -37,15 +44,17 @@ const Header = ({ siteTitle, setMobileMenuOpen }) => {
             </button>
           </div>
         </div>
-        <animated.h1
-          className="ah-header-title ah-header-title-right"
-          style={titleRightStyle}
-        >
-          <Link to="/" className="ah-header-title-link">
-            {/* {siteTitle} */}
-            HELDRING
-          </Link>
-        </animated.h1>
+        <div ref={titleLowerRef}>
+          <animated.h1
+            className="ah-header-title ah-header-title-right"
+            style={titleRightStyle}
+          >
+            <Link to="/" className="ah-header-title-link">
+              {/* {siteTitle} */}
+              HELDRING
+            </Link>
+          </animated.h1>
+        </div>
       </div>
     </header>
   )
