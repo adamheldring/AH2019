96a9ad86b4e5a1ea133066d77d25f48377e93fe9
A-Game <javascript@adamheldring.com>
2019-12-15 14:19:07 +0100

    make use of gatsby-image

--- code/src/pages/bio.js
+++ code/src/pages/bio.js
@@ -1,21 +1,37 @@
 import React, { Fragment } from 'react'
-import { Link } from 'gatsby'
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
 import Image from '../components/image'
 
-import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const SecondPage = () => (
-  <Fragment>
-    <SEO title="biography" />
-    <h1>Bio</h1>
-    <p>Thinking about New York...</p>
+const SecondPage = ({ data }) => {
+  console.log(data)
+  return (
+    <Fragment>
+      <SEO title="biography" />
+      <h1>Bio</h1>
+      <p>Thinking about New York...</p>
 
-    <div className="ah-page">
-      <h5>(Gatsby image query example)</h5>
-      <Image />
-    </div>
-  </Fragment>
-)
+      <div className="ah-page">
+        <h5>(Gatsby image query example)</h5>
+        <Img fluid={data.logo.childImageSharp.fluid} />
+        <Image />
+      </div>
+    </Fragment>
+  )
+}
 
 export default SecondPage
+
+export const logoQuery = graphql`
+  query {
+    logo: file(relativePath: { eq: "preston.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
