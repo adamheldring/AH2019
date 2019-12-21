ad39378ff6103e054cc9cf23d4678955daa939e0
A-Game <javascript@adamheldring.com>
2019-12-21 13:12:01 +0100

    wip - structure longer article section

--- code/src/pages/index.js
+++ code/src/pages/index.js
@@ -26,8 +26,8 @@ const IndexPage = ({ data }) => (
         </section>
       </ArticleWrapper>
       <ArticleWrapper title="LATEST RELEASE" split>
-        <section className="ah-article-visual--image-outer-container ah-article--split-section">
-          <div className="ah-article-visual--image-inner-container">
+        <section className="ah-article-visual--image-square-outer-container ah-article--split-section">
+          <div className="ah-article-visual--image-square-inner-container">
             <Img fluid={data.ymcover.childImageSharp.fluid} />
           </div>
         </section>
@@ -49,7 +49,34 @@ const IndexPage = ({ data }) => (
           </p>
         </section>
       </ArticleWrapper>
-      <ArticleWrapper title="ABOUT"></ArticleWrapper>
+      <ArticleWrapper title="ABOUT">
+        <section className="ah-article-visual--image-wide-container">
+          <Img fluid={data.portrait.childImageSharp.fluid} />
+        </section>
+        <section className="ah-article-info-wrapper">
+          <p className="ah-article-info-paragraph ah-article-info-paragraph--block">
+            <h2 className="ah-article-info-paragraph-heading">Biography</h2>
+            <span>
+              <b>Adam Heldring</b> released two Ep:s and the debut album ”Mount
+              Foreverest” in quick succession which generated wide interest both
+              at home in Sweden and on international ground. More than a hundred
+              live shows as well as TV and radio appearances followed in the US,
+              UK, Sweden, Norway and Finland. He was nominated for the Swedish
+              music prize ”Rockbjörnen” and played festivals in New York, Texas
+              and Brighton. The latest release ”True North EP” from 2010
+              including the indie-hit ”Bear Town” was crowned with a series of
+              sold out theater shows at the City Theater in his home town
+              Eskilstuna among other places. <br />
+              <br />
+              Then things suddenly went quiet. Heldring announced a break from
+              the spotlight and spent months on the move in Los Angeles, Berlin
+              and Paris – constantly writing on the music for his second album.
+              The record has come together during a series of silent years
+              behind locked studio doors and under great secrecy.
+            </span>
+          </p>
+        </section>
+      </ArticleWrapper>
       <ArticleWrapper title="SOCIAL MEDIA"></ArticleWrapper>
     </div>
   </Fragment>
@@ -59,8 +86,17 @@ export default IndexPage
 
 export const imageQuery = graphql`
   query {
-    ymcover: file(
-      relativePath: { eq: "materials/covers/ah-youmore-single-cover.jpg" }
+    ymcover: file(relativePath: { eq: "covers/ah-youmore-single-cover.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    portrait: file(
+      relativePath: {
+        eq: "press/Adam_Heldring_press_2019_1_-_Photo_Andreas_Karlsson_(highres).jpg"
+      }
     ) {
       childImageSharp {
         fluid {
