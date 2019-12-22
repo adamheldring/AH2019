8f551388fba763bd3364f376b8dd11f49e6bae92
A-Game <javascript@adamheldring.com>
2019-12-22 10:59:04 +0100

    minor fix to article layout

--- code/src/pages/index.js
+++ code/src/pages/index.js
@@ -55,24 +55,37 @@ const IndexPage = ({ data }) => (
         </section>
         <section className="ah-article-info-wrapper">
           <p className="ah-article-info-paragraph ah-article-info-paragraph--block">
-            <h2 className="ah-article-info-paragraph-heading">Biography</h2>
+            <h2 className="ah-article-info-paragraph-heading ah-article-info-paragraph-heading--space">
+              Biography
+            </h2>
             <span>
-              <b>Adam Heldring</b> released two Ep:s and the debut album ”Mount
-              Foreverest” in quick succession which generated wide interest both
-              at home in Sweden and on international ground. More than a hundred
-              live shows as well as TV and radio appearances followed in the US,
-              UK, Sweden, Norway and Finland. He was nominated for the Swedish
-              music prize ”Rockbjörnen” and played festivals in New York, Texas
-              and Brighton. The latest release ”True North EP” from 2010
-              including the indie-hit ”Bear Town” was crowned with a series of
-              sold out theater shows at the City Theater in his home town
-              Eskilstuna among other places. <br />
+              <b>
+                Adam Heldring released two Ep:s and the debut album ”Mount
+                Foreverest” in quick succession which generated wide interest
+                both at home in Sweden and on international ground.
+              </b>
+              <br />
+              <br />
+              More than a hundred live shows as well as TV and radio appearances
+              followed in the US, UK, Sweden, Norway and Finland. He was
+              nominated for the Swedish music prize ”Rockbjörnen” and played
+              festivals in New York, Texas and Brighton. The latest release
+              ”True North EP” from 2010 including the indie-hit ”Bear Town” was
+              crowned with a series of sold out theater shows at the City
+              Theater in his home town Eskilstuna among other places. <br />
               <br />
               Then things suddenly went quiet. Heldring announced a break from
               the spotlight and spent months on the move in Los Angeles, Berlin
               and Paris – constantly writing on the music for his second album.
               The record has come together during a series of silent years
-              behind locked studio doors and under great secrecy.
+              behind locked studio doors and under great secrecy. <br />
+              <br />
+              <i>
+                {/* <b> */}
+                The first single "You/More" was released in 2019 and the full
+                second album is expected in 2020.
+                {/* </b> */}
+              </i>
             </span>
           </p>
         </section>
