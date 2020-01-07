c0efc95c655237d758664977b06cc54eef003ab2
A-Game <javascript@adamheldring.com>
2020-01-07 20:14:33 +0100

    style footer

--- code/src/components/Footer/Footer.jsx
+++ code/src/components/Footer/Footer.jsx
@@ -10,32 +10,66 @@ const Footer = ({ labelLogo }) => {
     <footer className="ah-footer">
       <div className="ah-footer-content">
         <div className="ah-footer-content-box">
-          <a className="ah-footer-link" href="http://www.tennismusic.net">
+          <a
+            className="ah-footer-tennis-logo"
+            href="http://www.tennismusic.net"
+            style={{ height: '26px' }}
+            alt="Tennis Music"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <Img fixed={labelLogo.childImageSharp.fixed} />
           </a>
-          <span>Tennis Music</span>
-          <span>tennismusic.net</span>
-          <span>© {new Date().getFullYear()}</span>
-        </div>
-        <div className="ah-footer-content-box ah-footer-content-box--center">
-          <span>
-            <b>INFO</b>
+          <span className="test">
+            <a
+              className="ah-footer-link"
+              href="http://www.tennismusic.net"
+              alt="Tennis Music"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Tennis Music
+            </a>
           </span>
-          {menuData.submenu.map(submenuItem => (
-            <Link to={submenuItem.url} className="ah-footer-link">
-              {submenuItem.title}
-            </Link>
-          ))}
-        </div>
-        <div className="ah-footer-content-box ah-footer-content-box--right">
-          <span>
-            <b>MENU</b>
+          <span className="test">
+            <a
+              className="ah-footer-link"
+              href="http://www.tennismusic.net"
+              alt="Tennis Music"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              www.tennismusic.net
+            </a>
           </span>
-          {menuData.menu.map(menuItem => (
-            <Link to={menuItem.url} className="ah-footer-link">
-              {menuItem.title}
-            </Link>
-          ))}
+          <span className="test">
+            <a
+              to="info@tennismusic.net"
+              className="ah-footer-link"
+              href="mailto:info@tennismusic.net"
+            >
+              info@tennismusic.net
+            </a>
+          </span>
+          <span className="test">© {new Date().getFullYear()}</span>
+        </div>
+        <div className="ah-footer-menu-container">
+          <div className="ah-footer-content-box ah-footer-content-box--center">
+            <span className="ah-footer-content-box-heading">INFO</span>
+            {menuData.submenu.map(submenuItem => (
+              <Link to={submenuItem.url} className="ah-footer-link">
+                {submenuItem.title}
+              </Link>
+            ))}
+          </div>
+          <div className="ah-footer-content-box ah-footer-content-box--right">
+            <span className="ah-footer-content-box-heading">MENU</span>
+            {menuData.menu.map(menuItem => (
+              <Link to={menuItem.url} className="ah-footer-link">
+                {menuItem.title}
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </footer>
