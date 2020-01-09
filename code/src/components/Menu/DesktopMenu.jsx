f84aca766a70a0577af4b9fea7a834e0eb572fa9
A-Game <javascript@adamheldring.com>
2020-01-09 13:08:32 +0100

    add temp menu item for submenu pages and add transition to desktop menu hover

--- code/src/components/Menu/DesktopMenu.jsx
+++ code/src/components/Menu/DesktopMenu.jsx
@@ -2,23 +2,45 @@ import React from 'react'
 import { Link } from 'gatsby'
 import menuData from '../../../data/menuData'
 
-const Menu = ({ currentUri, menuInDarkmode }) => (
-  <div className="ah-menu">
-    <ul className="ah-menu-list">
-      {menuData.menu.map((menuItem, index) => (
-        <li key={`${index}-${menuItem.title}`}>
-          <Link
-            to={menuItem.url}
-            className={`ah-menu-list-item ${
-              menuItem.url === currentUri ? 'ah-menu-list-item--active' : ''
-            } ${menuInDarkmode ? 'ah-menu-list-item--darkmode' : ''}`}
+const Menu = ({ currentUri, menuInDarkmode }) => {
+  const activeSubmenuItem = menuData.submenu.find(
+    submenuItem => submenuItem.url === currentUri
+  )
+  return (
+    <div className="ah-menu">
+      <ul className="ah-menu-list">
+        {activeSubmenuItem && (
+          <li className="ah-menu-list-item-wrapper">
+            <Link
+              to={activeSubmenuItem.url}
+              className={`ah-menu-list-item ah-menu-list-submenu-item--active ${
+                menuInDarkmode
+                  ? 'ah-menu-list-submenu-item--active--darkmode'
+                  : ''
+              }`}
+            >
+              {activeSubmenuItem.title.toUpperCase()}
+            </Link>
+          </li>
+        )}
+        {menuData.menu.map((menuItem, index) => (
+          <li
+            key={`${index}-${menuItem.title}`}
+            className="ah-menu-list-item-wrapper"
           >
-            {menuItem.title.toUpperCase()}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </div>
-)
+            <Link
+              to={menuItem.url}
+              className={`ah-menu-list-item ${
+                menuItem.url === currentUri ? 'ah-menu-list-item--active' : ''
+              } ${menuInDarkmode ? 'ah-menu-list-item--darkmode' : ''}`}
+            >
+              {menuItem.title.toUpperCase()}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
 export default Menu
