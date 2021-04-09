ef0640a6bfe64c6279ddef3335bd6e6d7efb762f
A-Game <javascript@adamheldring.com>
2021-04-09 10:53:17 +0200

    update snippet article with golden state release date

--- code/src/components/Article/SnippetArticle.jsx
+++ code/src/components/Article/SnippetArticle.jsx
@@ -4,32 +4,13 @@ import scrollToTop from '../../helpers/scrollToTop'
 import ArticleWrapper from './ArticleWrapper/ArticleWrapper'
 
 const SnippetArticle = () => (
-  <ArticleWrapper title="LATEST NEWS" singlePageLink="">
-    <section className="ah-article-visual--video-container">
-      <iframe
-        title="Dance (Teaser)"
-        className="ah-article-visual--video"
-        src="https://www.youtube.com/embed/3rEP14ghceE?loop=1&playList=3rEP14ghceE&modestbranding"
-        frameBorder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
-    </section>
+  <ArticleWrapper title="LATEST NEWS" singlePageLink="/golden-state-single">
     <section className="ah-article-info-wrapper">
       <div className="ah-article-info-paragraph ah-article-info-paragraph--list">
         <h2 className="ah-article-info-paragraph-heading">
-          New single DANCE out now!
+          New single GOLDEN STATE out April 16
         </h2>
-        <a
-          href="spotify:album:7lpHjjvrMggNhHpxIINvFr"
-          alt="Open Dance single on Spotify"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="ah-snippet-link"
-        >
-          LISTEN NOW ON SPOTIFY
-        </a>
-        <Link onClick={scrollToTop} to="/dance-single-out-now">
+        <Link onClick={scrollToTop} to="/golden-state-single">
           <span className="ah-contact-link">
             <i>Read full article</i>
           </span>
@@ -38,5 +19,40 @@ const SnippetArticle = () => (
     </section>
   </ArticleWrapper>
 )
+// const SnippetArticle = () => (
+//   <ArticleWrapper title="LATEST NEWS" singlePageLink="">
+//     <section className="ah-article-visual--video-container">
+//       <iframe
+//         title="Dance (Teaser)"
+//         className="ah-article-visual--video"
+//         src="https://www.youtube.com/embed/3rEP14ghceE?loop=1&playList=3rEP14ghceE&modestbranding"
+//         frameBorder="0"
+//         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+//         allowFullScreen
+//       ></iframe>
+//     </section>
+//     <section className="ah-article-info-wrapper">
+//       <div className="ah-article-info-paragraph ah-article-info-paragraph--list">
+//         <h2 className="ah-article-info-paragraph-heading">
+//           New single DANCE out now!
+//         </h2>
+//         <a
+//           href="spotify:album:7lpHjjvrMggNhHpxIINvFr"
+//           alt="Open Dance single on Spotify"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//           className="ah-snippet-link"
+//         >
+//           LISTEN NOW ON SPOTIFY
+//         </a>
+//         <Link onClick={scrollToTop} to="/dance-single-out-now">
+//           <span className="ah-contact-link">
+//             <i>Read full article</i>
+//           </span>
+//         </Link>
+//       </div>
+//     </section>
+//   </ArticleWrapper>
+// )
 
 export default SnippetArticle
--- code/src/pages/news/2021-04-09-golden-state-single/index.md
+++ code/src/pages/news/2021-04-09-golden-state-single/index.md
@@ -1,11 +1,11 @@
 ---
 path: "/golden-state-single"
 date: "2021-04-09"
-title: "New single GOLDEN STATE out Apr 16"
+title: "New single GOLDEN STATE out next Friday April 16"
 featuredImage: "ah-golden_state-single-cover.jpg"
 photoCredit: ""
 ---
 
-### "Golden State" single will be released next Friday April 16.
+### The last single ahead of album realease "Golden State" will be released next Friday April 16, one week from today.
 
 The single is taken from the upcoming second album and will be released globally on all platforms by Tennis Music.
