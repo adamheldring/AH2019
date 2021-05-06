2fd837b18d8eae7e9a13a198c55720f711ce0b1d
A-Game <javascript@adamheldring.com>
2021-05-06 22:16:05 +0200

    update cover art article with PoC cover

--- code/src/components/Article/CoverArtArticle.jsx
+++ code/src/components/Article/CoverArtArticle.jsx
@@ -4,17 +4,27 @@ import ArticleWrapper from './ArticleWrapper/ArticleWrapper'
 import DownloadbleImageCollection, {
   buildDownloadableImageCollectionArray,
 } from '../DownloadbleImageCollection/DownloadableImageCollection'
-import coverArt1High from '../../images/covers/ah-golden-state-single-cover.jpg'
-import coverArt2High from '../../images/covers/ah-dance-single-cover.jpg'
-import coverArt3High from '../../images/covers/ah-youmore-single-cover.jpg'
-import coverArt4High from '../../images/covers/ah-true_north-ep-cover.jpg'
-import coverArt5High from '../../images/covers/ah-mount_foreverest-album-cover.jpg'
-import coverArt6High from '../../images/covers/ah-metro-ep-cover.jpg'
+import coverArt1High from '../../images/covers/ah-ports_of_call-album-cover.jpg'
+import coverArt2High from '../../images/covers/ah-golden-state-single-cover.jpg'
+import coverArt3High from '../../images/covers/ah-dance-single-cover.jpg'
+import coverArt4High from '../../images/covers/ah-youmore-single-cover.jpg'
+import coverArt5High from '../../images/covers/ah-true_north-ep-cover.jpg'
+import coverArt6High from '../../images/covers/ah-mount_foreverest-album-cover.jpg'
+import coverArt7High from '../../images/covers/ah-metro-ep-cover.jpg'
 
 const CoverArtArticle = () => {
   const data = useStaticQuery(graphql`
     query coverArtQuery {
       cover1: file(
+        relativePath: { eq: "covers/ah-ports_of_call-album-cover.jpg" }
+      ) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      cover2: file(
         relativePath: { eq: "covers/ah-golden-state-single-cover.jpg" }
       ) {
         childImageSharp {
@@ -23,28 +33,28 @@ const CoverArtArticle = () => {
           }
         }
       }
-      cover2: file(relativePath: { eq: "covers/ah-dance-single-cover.jpg" }) {
+      cover3: file(relativePath: { eq: "covers/ah-dance-single-cover.jpg" }) {
         childImageSharp {
           fluid {
             ...GatsbyImageSharpFluid
           }
         }
       }
-      cover3: file(relativePath: { eq: "covers/ah-youmore-single-cover.jpg" }) {
+      cover4: file(relativePath: { eq: "covers/ah-youmore-single-cover.jpg" }) {
         childImageSharp {
           fluid {
             ...GatsbyImageSharpFluid
           }
         }
       }
-      cover4: file(relativePath: { eq: "covers/ah-true_north-ep-cover.jpg" }) {
+      cover5: file(relativePath: { eq: "covers/ah-true_north-ep-cover.jpg" }) {
         childImageSharp {
           fluid {
             ...GatsbyImageSharpFluid
           }
         }
       }
-      cover5: file(
+      cover6: file(
         relativePath: { eq: "covers/ah-mount_foreverest-album-cover.jpg" }
       ) {
         childImageSharp {
@@ -53,7 +63,7 @@ const CoverArtArticle = () => {
           }
         }
       }
-      cover6: file(relativePath: { eq: "covers/ah-metro-ep-cover.jpg" }) {
+      cover7: file(relativePath: { eq: "covers/ah-metro-ep-cover.jpg" }) {
         childImageSharp {
           fluid {
             ...GatsbyImageSharpFluid
@@ -71,9 +81,17 @@ const CoverArtArticle = () => {
   // Create array with static download links and other image info that corresponds with above display array
   const imageInfoArray = [
     {
-      name: 'Golden State',
+      name: 'Ports Of Call',
       files: {
         highres: coverArt1High,
+      },
+      year: 2021,
+      description: 'Ports Of Call album cover art.',
+    },
+    {
+      name: 'Golden State',
+      files: {
+        highres: coverArt2High,
       },
       year: 2021,
       description: 'Golden State single cover art.',
@@ -81,7 +99,7 @@ const CoverArtArticle = () => {
     {
       name: 'Dance',
       files: {
-        highres: coverArt2High,
+        highres: coverArt3High,
       },
       year: 2021,
       description: 'Dance single cover art.',
@@ -89,7 +107,7 @@ const CoverArtArticle = () => {
     {
       name: 'You / More',
       files: {
-        highres: coverArt3High,
+        highres: coverArt4High,
       },
       year: 2019,
       description: 'You / More single cover art.',
@@ -97,7 +115,7 @@ const CoverArtArticle = () => {
     {
       name: 'True North EP',
       files: {
-        highres: coverArt4High,
+        highres: coverArt5High,
       },
       year: 2010,
       description: 'True North EP cover art.',
@@ -105,7 +123,7 @@ const CoverArtArticle = () => {
     {
       name: 'You / More',
       files: {
-        highres: coverArt5High,
+        highres: coverArt6High,
       },
       year: 2009,
       description: 'Mount Foreverest album cover art.',
@@ -113,7 +131,7 @@ const CoverArtArticle = () => {
     {
       name: 'Metro EP',
       files: {
-        highres: coverArt6High,
+        highres: coverArt7High,
       },
       year: 2009,
       description: 'Metro EP cover art.',
